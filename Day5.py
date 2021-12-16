967f71a6781f942b22a79287be69a5141049d635
Prakriti Pandey <95183905+Praks-23@users.noreply.github.com>
2021-12-16 18:23:44 +0530

    Password Generator

--- Day5.py
+++ Day5.py
@@ -9,6 +9,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
 # Welcome message and asking user for the input
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
